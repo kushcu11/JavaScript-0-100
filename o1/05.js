c25283119dd7f2d8b6a7ea07c4af83b2f4c09cba
Kushagra Pandey <ivanatinkledrexel@gmail.com>
2025-08-04 11:37:07 +0000

    voting problem 1

---
+++ o1/05.js
@@ -0,0 +1,23 @@
+//conditonal statement problem1
+
+let age = 20
+
+if(age>=18){
+    console.log("you are eligible to vote")
+}
+else{
+    console.log("you cant vote!!")
+}
+
+
+
+let age2 = 3
+
+if(age2>=18){
+    console.log("you are eligible to vote")
+}
+else{
+    console.log("you cant vote!!")
+}
+
+
